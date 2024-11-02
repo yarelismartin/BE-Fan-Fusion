68c5ef2a259b1b14ca3c5fc187a532de0621213c
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-01 19:40:49 -0500

    created repo interface for categories

--- Interfaces/ICategoryRepository.cs
+++ Interfaces/ICategoryRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace BE_Fan_Fusion.Interfaces
+﻿using BE_Fan_Fusion.Models;
+
+namespace BE_Fan_Fusion.Interfaces
 {
     public interface ICategoryRepository
     {
+        Task<List<Category>> GetCategoriesAsync();
     }
 }
