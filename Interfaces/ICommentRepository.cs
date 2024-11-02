7f0de33a27e4ded4407294ed4fbb716b315bf62d
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-01 19:35:46 -0500

    created repo interface for comment

--- Interfaces/ICommentRepository.cs
+++ Interfaces/ICommentRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace BE_Fan_Fusion.Interfaces
+﻿using BE_Fan_Fusion.Models;
+
+namespace BE_Fan_Fusion.Interfaces
 {
     public interface ICommentRepository
     {
+        Task<Comment> CreateCommentAsync(Comment comment);
+        Task<Comment> DeleteCommentAsync(int commentId);
     }
 }
