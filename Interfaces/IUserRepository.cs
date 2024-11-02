dc7071d964c8d6d4fb4c6f9dfa9fb2983f6554fe
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-01 19:23:35 -0500

    created repo interface for user

--- Interfaces/IUserRepository.cs
+++ Interfaces/IUserRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace BE_Fan_Fusion.Interfaces
+﻿using BE_Fan_Fusion.Models;
+
+namespace BE_Fan_Fusion.Interfaces
 {
     public interface IUserRepository
     {
+        Task<User> CheckUser(string userUid);
+        Task<User> GetUserById(int userId);
+
     }
 }
