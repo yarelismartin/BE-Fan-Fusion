0890c6b70d3986efdae8655a6ef6a6655b948961
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-01 19:19:18 -0500

    created repo interface for tag

--- Interfaces/ITagRepository.cs
+++ Interfaces/ITagRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace BE_Fan_Fusion.Interfaces
+﻿using BE_Fan_Fusion.Models;
+
+namespace BE_Fan_Fusion.Interfaces
 {
     public interface ITagRepository
     {
+        Task<List<Tag>> GetAllTagsAsync();
+        Task<Tag> GetTagByIdAsync(int tagId);
+
     }
 }
