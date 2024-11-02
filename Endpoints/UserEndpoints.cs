a3f8becc7a63a5f98ec0eb5e4fb112681e3a9cc3
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-02 12:22:01 -0500

    created the checkUser endpoint

--- Endpoints/UserEndpoints.cs
+++ Endpoints/UserEndpoints.cs
@@ -1,4 +1,5 @@
-﻿using BE_Fan_Fusion.Models;
+﻿using BE_Fan_Fusion.Data;
+using BE_Fan_Fusion.Models;
 
 namespace BE_Fan_Fusion.Endpoints
 {
@@ -7,6 +8,27 @@ namespace BE_Fan_Fusion.Endpoints
         public static void MapUserEndpoints(this IEndpointRouteBuilder routes)
         {
             var group = routes.MapGroup("").WithTags(nameof(User));
+
+            group.MapGet("/checkUser/{uid}", (FanFusionDbContext db, string uir) =>
+            {
+                User? userCheck = db.Users.FirstOrDefault(u => u.Uid == uid);
+
+                if (userCheck == null)
+                {
+                    return Results.NotFound("User is not registered");
+                }
+                return Results.Ok(new
+                {
+                    userCheck.Id,
+                    userCheck.FirstName,
+                    userCheck.LastName,
+                    userCheck.Email,
+                    userCheck.Image,
+                    userCheck.Uid
+                });
+            });
         }
+
+
     }
 }
