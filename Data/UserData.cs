c2dde6b4fd477ed8583cbf4abee6083bbe8e0094
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-11-02 11:56:44 -0500

    Updated Teammates user seeded data and added images to other users

--- Data/UserData.cs
+++ Data/UserData.cs
@@ -9,42 +9,42 @@ namespace BE_Fan_Fusion.Data
         new User
         {
             Id = 1,
-            FirstName = "John",
-            LastName = "Doe",
-            Username = "john_doe",
-            Email = "john@example.com",
-            Image = "https://example.com/images/john.jpg",
-            Uid = "user001"
+            FirstName = "Fletcher",
+            LastName = "Moore",
+            Username = "fletcher_moore",
+            Email = "fletcher@example.com",
+            Image = "https://ca.slack-edge.com/T03F2SDTJ-U06ANN13BJA-a6223b135600-512",
+            Uid = "spB8Zq4wELSQ2CYU8B85GAlo9UF3"
         },
         new User
         {
             Id = 2,
-            FirstName = "Jane",
-            LastName = "Smith",
-            Username = "jane_smith",
-            Email = "jane@example.com",
-            Image = "https://example.com/images/jane.jpg",
-            Uid = "user002"
+            FirstName = "Yarelis",
+            LastName = "Martin",
+            Username = "yarelis_martin",
+            Email = "yarelis@example.com",
+            Image = "https://ca.slack-edge.com/T03F2SDTJ-U06964LME06-e7ae012d8792-512",
+            Uid = "BoMhkHml16cdAERkecfxE2RRMqO2"
         },
         new User
         {
             Id = 3,
-            FirstName = "Alice",
-            LastName = "Johnson",
-            Username = "alice_johnson",
-            Email = "alice@example.com",
-            Image = "https://example.com/images/alice.jpg",
-            Uid = "user003"
+            FirstName = "Sirena",
+            LastName = "Foster",
+            Username = "sirena_foster",
+            Email = "sirrena@example.com",
+            Image = "https://ca.slack-edge.com/T03F2SDTJ-U079DCXBYHW-e4fd2297a065-512",
+            Uid = "MxzBtnidXFYcqBl2zw0unXm6Vbl2"
         },
         new User
         {
             Id = 4,
-            FirstName = "Bob",
-            LastName = "Brown",
-            Username = "bob_brown",
-            Email = "bob@example.com",
-            Image = "https://example.com/images/bob.jpg",
-            Uid = "user004"
+            FirstName = "Casey",
+            LastName = "Cunningham",
+            Username = "casey_cunningham",
+            Email = "casey@example.com",
+            Image = "https://ca.slack-edge.com/T03F2SDTJ-U078T3MSH0U-2ccec7b81ad3-512",
+            Uid = "t3CunL7C0nUSf9a28dcqBZs3J9y1"
         },
         new User
         {
@@ -63,7 +63,7 @@ namespace BE_Fan_Fusion.Data
             LastName = "Wilson",
             Username = "eve_wilson",
             Email = "eve@example.com",
-            Image = "https://example.com/images/eve.jpg",
+            Image = "https://i.pinimg.com/564x/2a/8a/6d/2a8a6d14195e800e21d58d25c96d6e4a.jpg",
             Uid = "user006"
         },
         new User
@@ -83,7 +83,7 @@ namespace BE_Fan_Fusion.Data
             LastName = "Taylor",
             Username = "fiona_taylor",
             Email = "fiona@example.com",
-            Image = "https://example.com/images/fiona.jpg",
+            Image = "https://preview.redd.it/generic-e-girl-face-v0-4hqohs5o1h6a1.jpg?width=640&crop=smart&auto=webp&s=bc432d97ba360c6412daa30e0cb5d00d25e35d1e",
             Uid = "user008"
         },
         new User
@@ -93,7 +93,7 @@ namespace BE_Fan_Fusion.Data
             LastName = "Anderson",
             Username = "george_anderson",
             Email = "george@example.com",
-            Image = "https://example.com/images/george.jpg",
+            Image = "https://media.istockphoto.com/id/1020474390/photo/displeased-mature-man.jpg?s=612x612&w=0&k=20&c=LgsEpwyAuPXh7VcYoQooNb7Jh7U7pl1l9xzwm0SC-is=",
             Uid = "user009"
         },
         new User
@@ -103,7 +103,7 @@ namespace BE_Fan_Fusion.Data
             LastName = "Thomas",
             Username = "hannah_thomas",
             Email = "hannah@example.com",
-            Image = "https://example.com/images/hannah.jpg",
+            Image = "https://thumbs.dreamstime.com/b/teen-girl-orange-shirt-10766265.jpg",
             Uid = "user890"
         },
         new User
@@ -113,7 +113,7 @@ namespace BE_Fan_Fusion.Data
             LastName = "Jackson",
             Username = "ian_jackson",
             Email = "ian@example.com",
-            Image = "https://example.com/images/ian.jpg",
+            Image = "https://media.istockphoto.com/id/181894143/photo/nervous-young-man.jpg?s=612x612&w=0&k=20&c=VGM3L-GNjuU6azlEu9HWgSR872iObXPJ81AyjUqD8AQ=",
             Uid = "user567"
         },
         new User
@@ -123,7 +123,7 @@ namespace BE_Fan_Fusion.Data
             LastName = "White",
             Username = "jack_white",
             Email = "jack@example.com",
-            Image = "https://example.com/images/jack.jpg",
+            Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa3HD9NGpcssX7w3fcMClAQtN6Xa638VkEHg&s",
             Uid = "user234"
         },
         new User
@@ -133,7 +133,7 @@ namespace BE_Fan_Fusion.Data
             LastName = "Garcia",
             Username = "kathy_garcia",
             Email = "kathy@example.com",
-            Image = "https://example.com/images/kathy.jpg",
+            Image = "https://thumbs.dreamstime.com/b/woman-wearing-black-generic-glasses-happy-43745660.jpg",
             Uid = "user789"
         },
         new User
@@ -143,7 +143,7 @@ namespace BE_Fan_Fusion.Data
             LastName = "Martinez",
             Username = "liam_martinez",
             Email = "liam@example.com",
-            Image = "https://example.com/images/liam.jpg",
+            Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAnWbXLAOuwYf2v-B7jQedXh_QVlUNPoqdhEpxc_OC5BeSx7YRtvaDlebgEpxBIg_Qr9s&usqp=CAU",
             Uid = "user456"
         },
         new User
@@ -153,7 +153,7 @@ namespace BE_Fan_Fusion.Data
             LastName = "Hernandez",
             Username = "mia_hernandez",
             Email = "mia@example.com",
-            Image = "https://example.com/images/mia.jpg",
+            Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS24W-X-ipaKgDC5RYy4mUbOh6SpAExS7_7gIVeN1nc-dud4ybbFEfj0qcP6PIF4dh3I3I&usqp=CAU",
             Uid = "user123"
         }
     };
