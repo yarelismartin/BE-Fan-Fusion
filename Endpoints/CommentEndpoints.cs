4f7889de61d212a409f1ae74cab658393ca57285
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-04 16:33:46 -0600

    create comment endpoint finalized. Had to add additional methods to the repository interface in order to check if the user and chapter exist before going adding the comment to the database. Added a try catch to the endpoint so if it catched any of the argument exceptions coming from the comment service file it will be passed to the endpoint and shown to the user

--- Endpoints/CommentEndpoints.cs
+++ Endpoints/CommentEndpoints.cs
@@ -1,4 +1,7 @@
-﻿using BE_Fan_Fusion.Models;
+﻿using BE_Fan_Fusion.Data;
+using BE_Fan_Fusion.Interfaces;
+using BE_Fan_Fusion.Models;
+using Microsoft.AspNetCore.Builder;
 
 namespace BE_Fan_Fusion.Endpoints
 {
@@ -6,7 +9,20 @@ namespace BE_Fan_Fusion.Endpoints
     {
         public static void MapCommentEndpoints(this IEndpointRouteBuilder routes)
         {
-            var group = routes.MapGroup("").WithTags(nameof(Comment));
+            var group = routes.MapGroup("comments").WithTags(nameof(Comment));
+
+            group.MapPost("/", async (ICommentService commentService, Comment newComment) =>
+            {
+                try
+                {
+                    var createdComment = await commentService.CreateCommentAsync(newComment);
+                    return Results.Ok(createdComment);
+                }
+                catch (ArgumentException ex)
+                {
+                    return Results.NotFound(ex.Message);
+                }
+            });
         }
     }
 }
--- Repositories/CommentRepository.cs
+++ Repositories/CommentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using BE_Fan_Fusion.Data;
 using BE_Fan_Fusion.Interfaces;
 using BE_Fan_Fusion.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BE_Fan_Fusion.Repositories
 {
@@ -16,7 +17,18 @@ namespace BE_Fan_Fusion.Repositories
 
         public async Task<Comment> CreateCommentAsync(Comment comment)
         {
-            throw new NotImplementedException();
+            await dbContext.Comments.AddAsync(comment);
+            await dbContext.SaveChangesAsync();
+            return comment;
+        }
+        public async Task<bool> UserExistsAsync(int userId)
+        {
+            return await dbContext.Users.AnyAsync(user => user.Id == userId);
+        }
+
+        public async Task<bool> ChapterExistsAsync(int chapterId)
+        {
+            return await dbContext.Chapters.AnyAsync(chapter => chapter.Id == chapterId);
         }
         public async Task<Comment> DeleteCommentAsync(int commentId)
         {
