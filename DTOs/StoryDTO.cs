3bd69fde245ad090c92b2a84303b430dd4b2a078
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-05 17:09:15 -0600

    updated the storyDTO

--- DTOs/StoryDTO.cs
+++ DTOs/StoryDTO.cs
@@ -8,12 +8,14 @@ namespace BE_Fan_Fusion.DTO
         public string Title { get; set; }
         public string Image { get; set; }
         public DateTime DateCreated { get; set; }
-        public StoryDTO(Story story)
+        public bool IsFavorited { get; set; }
+        public StoryDTO(Story story, bool isFavorited)
         {
             Id = story.Id;
             Title = story.Title;
             Image = story.Image;
             DateCreated = story.DateCreated;
+            IsFavorited = isFavorited;
         }
     }
 }
