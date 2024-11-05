f3313453e5da176b0e3dd9d0903eabe9c4cfad38
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-05 14:26:00 -0600

    Implement a dynamic  many-to-many relationship between User and Story for favoriting functionality using a join table

--- Data/FanFusionDbContext.cs
+++ Data/FanFusionDbContext.cs
@@ -19,17 +19,33 @@ namespace BE_Fan_Fusion.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-          modelBuilder.Entity<Story>().HasData(StoryData.Stories);
+            modelBuilder.Entity<User>()
+                    // Configures the User entity to have many FavoritedStories
+                    .HasMany(u => u.FavoritedStories)
+                    // Configures the relationship such that each Story can be favorited by many Users
+                    .WithMany(s => s.FavoritedByUsers)
+                    // Specifies that the join table won’t have a separate class; instead, it will just store key-value pairs for the foreign keys.
+                    .UsingEntity<Dictionary<string, object>>(
+                        "UserFavoriteStory", // The name of the join table
+                        j => j.HasOne<Story>() // Configures the join entity to reference the Story
+                            .WithMany() // Specifies that the Story can have many User favorites
+                            .HasForeignKey("StoryId"), // Defines the foreign key for the Story
+                        j => j.HasOne<User>() // Configures the join entity to reference the User
+                            .WithMany() // Specifies that the User can have many favorited Stories
+                            .HasForeignKey("UserId") // Defines the foreign key for the User
+                    );
 
-          modelBuilder.Entity<Tag>().HasData(TagData.Tags);
+              modelBuilder.Entity<Story>().HasData(StoryData.Stories);
 
-          modelBuilder.Entity<User>().HasData(UserData.Users);
+              modelBuilder.Entity<Tag>().HasData(TagData.Tags);
 
-          modelBuilder.Entity<Chapter>().HasData(ChapterData.Chapters);
+              modelBuilder.Entity<User>().HasData(UserData.Users);
 
-          modelBuilder.Entity<Comment>().HasData(CommentData.Comments);
+              modelBuilder.Entity<Chapter>().HasData(ChapterData.Chapters);
 
-          modelBuilder.Entity<Category>().HasData(CategoryData.Categories);
+              modelBuilder.Entity<Comment>().HasData(CommentData.Comments);
+
+              modelBuilder.Entity<Category>().HasData(CategoryData.Categories);
 
         }
 
