b3b0ba752231bd4088d809682fffcd84d8b241c6
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-05 14:49:21 -0600

    added a new migrartion to update database

---
+++ Migrations/20241105204436_AddUserFavoriteStory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BE_Fan_Fusion.Migrations
+{
+    public partial class AddUserFavoriteStory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFavoriteStory",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    StoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFavoriteStory", x => new { x.UserId, x.StoryId });
+                    table.ForeignKey(
+                        name: "FK_UserFavoriteStory_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserFavoriteStory_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavoriteStory_StoryId",
+                table: "UserFavoriteStory",
+                column: "StoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFavoriteStory");
+        }
+    }
+}
